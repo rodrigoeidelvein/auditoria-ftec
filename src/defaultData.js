bbacd2fad0efec01988b65172c219ad81815fbb7
Rodrigo Fazenda Eidelvein <rodrigoeidelvein@gmail.com>
2020-09-22 18:32:39 -0300

    Added default data and front page

---
+++ src/defaultData.js
@@ -0,0 +1,115 @@
+export default [
+  {
+    value: "A política é efetivamente comunicada aos usuários?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      "Existe cargo e funcionário responsável pela administração de segurança e acessos?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      "A Política é revisada periodicamente pelo administrador de segurança e atualizada conforme necessidade?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      "Os usuários são responsáveis pelas atividades executadas com sua senha?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      "Acesso a programas que podem adicionar, modificar ou apagar dados são restritos a um número mínimo de pessoas?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      " O administrador de acessos é avisado quanto à necessidade de desativação, ou troca de atribuição sobre a senha do usuário?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      "Escolha aleatoriamente 03 usuários desligados da empresa e verifique nos logs se há alguma tentativa de acesso",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value: "Usuários mantém a confidencialidade das suas senhas?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value: "Senhas estão sujeitas a complexidade e mudanças periódicas?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value:
+      "O software de controle registra acessos e tentativas mal sucedidas?",
+    program: "Auditoria de Acesso",
+  },
+  {
+    value: "Acesso do usuário é derrubado após tempo ocioso?",
+    program: "Auditoria de Acesso",
+  },
+
+  {
+    value:
+      "Avalie se existe supervisão e revisão de atividades para cada turno de processamento",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "Verifique se é exigido o registro de todas as rotinas no registro de operações",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "A inicialização e desligamento dos equipamentos são realizadas por pessoas autorizadas?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "Existe software de controle de acesso que limite a leitura e execução das bibliotecas?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "São utilizados rótulos internos e externos para identificação de arquivos?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "Verifique se existe relatório de ocorrências para documentar todas as exceções de processamento e respectivas ações de correção",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "Há um software que minimize as ações da área de operação automatizando o processo?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "Há um software que efetue o balanceamento de dados entre arquivos durante o processamento?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value: "Existe verificações chave para entrada de dados críticos?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value: "Verifique se existe uma central de atendimento aos usuários",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value: "A central de apoio é de conhecimento e utilização de todos?",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value:
+      "Verifique se existe procedimentos formais para registro de problemas ocorridos e correção dos mesmos",
+    program: "Auditoria de Operação do Computador",
+  },
+  {
+    value: "É utilizado algum controle para retenção e registro de impressão?",
+    program: "Auditoria de Operação do Computador",
+  },
+];
